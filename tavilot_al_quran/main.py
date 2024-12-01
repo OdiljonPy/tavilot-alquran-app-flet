a380533a52fd980241a5995d0af8df93618e23ed
abbosbek <hikmatullaevabbos24@gmail.com>
2024-11-30 21:13:42 -0500

    registration page added near login page

--- tavilot_al_quran/main.py
+++ tavilot_al_quran/main.py
@@ -4,7 +4,6 @@ import os
 from utils.validations import limit_length
 import requests
 from utils.common_variables import password
-from pages.register_page import registration_page
 from pages.home_page import home
 
 
@@ -28,13 +27,112 @@ def main(page: ft.Page):
     )
     ))
 
-    time.sleep(4)
+    time.sleep(2)
 
     page.clean()
     page.adaptive = True
     page.window_min_width = 1250
     page.window_min_height = 800
     page.theme_mode = ft.ThemeMode.LIGHT
+    #-------------------------------------------------------------------------------------------------------------------
+
+    def registration_response(phone, passw):
+        url = "https://alquran.zerodev.uz/api/v1/auth/register/"
+        headers = {
+            "Content-Type": "application/json",
+        }
+        phone_number = f'+998{phone.value}'
+        data = {
+            "phone_number": phone_number,
+            'password': passw.value
+        }
+        response = requests.post(url, json=data, headers=headers)
+        print(response.json())
+        otp_key = response.json().get('result')
+        if response.status_code == 200:
+            pass
+
+        else:
+            print('Error')
+        page.update()
+
+    def registration_page(e):
+        page.clean()
+        TC = '#E9BE5F'
+
+        phone_input = ft.TextField(
+            label='Phone number',
+            prefix_text="+998 ",
+            keyboard_type=ft.KeyboardType.NUMBER,
+            width=400,
+            on_change=lambda e: limit_length(phone_input, page),
+            border_width=2,
+            border_radius=10,
+            border_color=TC,
+            text_style=ft.TextStyle(weight="bold")
+        )
+        # Create a Column for all the fields on the left side
+        registration = ft.Container(content=ft.Row(
+            adaptive=True,
+            controls=[ft.Column(
+                adaptive=True,
+                controls=[
+                    ft.Text(value="  TA'VILOT \nAL-QURON \n", color=TC, style="displayLarge"),
+                    ft.Text(value="Assalomu alaykum! \nDavom etish uchun ro'yxatdan o'ting\n", width=400, weight='bold',
+                            style="titleLarge"),
+                    phone_input,
+                    password,
+                    ft.Text(),
+                    ft.OutlinedButton(
+                        text="Davom etish",
+                        on_click=lambda e: registration_response(phone_input, password),
+                        # Link the button to validation
+                        width=400,
+                        height=60,
+                        style=ft.ButtonStyle(
+                            color='white',
+                            bgcolor=TC,
+                            shape=ft.RoundedRectangleBorder(radius=8),
+                        )
+                    ),
+                    ft.OutlinedButton(
+                        text="Parol orqali kirish",
+                        on_click=lambda e: main(page),
+                        # Link the button to validation
+                        width=400,
+                        height=60,
+                        style=ft.ButtonStyle(
+                            color='white',
+                            bgcolor=TC,
+                            shape=ft.RoundedRectangleBorder(radius=8),
+
+                        ),
+                    ),
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            ),
+                ft.Container(
+                    content=ft.Image(
+                        src=os.path.abspath("assets/tavilot_book.png"),
+                        width=700,
+                        height=900,
+                        fit=ft.ImageFit.COVER,
+                        border_radius=100
+                    ),
+                    clip_behavior=ft.ClipBehavior.ANTI_ALIAS,  # Enable anti-aliasing for smoother edges
+                ),
+            ],
+            alignment=ft.MainAxisAlignment.SPACE_EVENLY,  # Spread content and image to left and right
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+        ),
+            adaptive=True,
+            expand=True,
+            alignment=ft.alignment.center
+        )
+        page.add(registration)
+
+    #-----Registration page---------------------------------------------------------------------------------------------
 
     # result = ft.Text(value="", color="red")
     TC = '#E9BE5F'
@@ -105,7 +203,7 @@ def main(page: ft.Page):
             ),
             ft.OutlinedButton(
                 text="Ro'yxatdan o'tish",
-                on_click=lambda e: registration_page(page),
+                on_click=registration_page,
                 # Link the button to validation
                 width=400,
                 height=60,
