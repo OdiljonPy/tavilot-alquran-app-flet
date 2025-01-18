be76d81b4b351db58e7e069d63ea64d6d639968d
abbosbek <hikmatullaevabbos24@gmail.com>
2025-01-17 20:14:42 -0500

    access token of user removed before accessing

---
+++ tavilot_al_quran/pages/pages_utils/appbar_search.py
@@ -0,0 +1,94 @@
+import flet as ft
+from ..al_quran_oquvchilariga import al_quron_oquvchilariga
+from ..surah_page import surah_page
+from ..menuscript import menuscript
+from ..studies import studies
+from ..resources import resources
+from ..refusal import refusal
+from ..home_page import home
+import os
+
+
+
+
+routes = {
+        "Abu Mansur Motrudiy": al_quron_oquvchilariga,
+        "Tavilot al-Quron": surah_page,
+        "Qo'lyozma va sharhlar": menuscript,
+        "Zamonaviy tadqiqotlar": studies,
+        "Resurslar": resources,
+        "Mutaassib oqimlarga raddiyalar": refusal
+    }
+
+active_route = None
+
+def navigate(e, route, page):
+    global active_route
+    if route != active_route:
+        active_route = route
+        # update_appbar(page)  # Refresh the AppBar with updated colors
+        if route in routes:
+            routes[route](page)  # Call the corresponding route function
+        else:
+            page.add(ft.Text("404 - Page Not Found", size=20))
+            page.update()
+
+def generate_appbar_actions(page):
+    return [
+        ft.TextButton(
+            adaptive=True,
+            expand=True,
+            text=route_label,
+            style=ft.ButtonStyle(
+                padding=0,
+                text_style=ft.TextStyle(size=15),
+                color='#007577' if route == active_route else ft.colors.BLACK,
+            ),
+            on_click=lambda e, r=route: navigate(e, r, page)
+        )
+        for route, route_label in [
+            ("Abu Mansur Motrudiy", "Abu Mansur Motrudiy"),
+            ("Tavilot al-Quron", "Tavilot al-Quron"),
+            ("Qo'lyozma va sharhlar", "Qo'lyozma va sharhlar"),
+            ("Zamonaviy tadqiqotlar", "Zamonaviy tadqiqotlar"),
+            ("Resurslar", "Resurslar"),
+            ("Mutaassib oqimlarga raddiyalar", "Mutaassib oqimlarga raddiyalar")
+
+        ]
+    ]
+
+def update_appbar(page):
+    page.appbar = ft.AppBar(
+        title=ft.Row(
+            alignment=ft.MainAxisAlignment.CENTER,
+            expand=True,
+            adaptive=True,
+            controls=[
+                *generate_appbar_actions(page),
+            ]
+        ),
+        center_title=True,
+        adaptive=True,
+        leading_width=100,
+        leading=ft.Container(
+            on_click=lambda e: home(page),
+            content=ft.Image(
+                expand=True,
+                color='#007577',
+                src=os.path.abspath("assets/tA'VILOT_Монтажная_область1.svg")
+            )),
+        actions=[
+            ft.Row(
+                adaptive=True,
+                expand=True,
+                alignment=ft.MainAxisAlignment.CENTER,
+                spacing=15,  # Reduced spacing to allow more room for items
+                controls=[
+                    # search
+                ],
+            ),
+        ],
+        bgcolor='white',
+        toolbar_height=80,
+    )
+
--- tavilot_al_quran/pages/surah_page.py
+++ tavilot_al_quran/pages/surah_page.py
@@ -9,15 +9,9 @@ TC = '#E9BE5F'
 def surah_page(page):
     page.clean()
     page.scroll = False
-    from .home_page import home
     from tavilot_al_quran.main import main
     from .html_pdf_handler import render_description
-    from .al_quran_oquvchilariga import al_quron_oquvchilariga
-    from .menuscript import menuscript
-    from .studies import studies
-    from .refusal import refusal
-    from .resources import resources
-    # from .payment_page import payment_page
+    from .pages_utils.appbar_search import update_appbar
     loading = ft.ProgressRing(color=TC)
     page.add(ft.Container(
         expand=True,
@@ -1073,198 +1067,99 @@ def surah_page(page):
         ],
         spacing=0
     )
-    back_button = ft.OutlinedButton(
-        content=ft.Row(controls=[
-            ft.Icon(ft.icons.ARROW_BACK, color='black', size=20),
-            ft.Text('Orqaga qaytish', color='black')
-        ]),
-        height=40,
-        width=170,
-        style=ft.ButtonStyle(
-            shape=ft.RoundedRectangleBorder(radius=10),
-            side=ft.BorderSide(color=TC, width=1),
-            bgcolor='white'
 
-        ),
-        adaptive=True,
-        on_click=lambda e: home(page),
-    )
-
-    def fetch_data(query):
-        url = f"http://176.221.28.202:8008/api/v1/search/?q={query}&search_type={button_number}"
-        headers = ""
-        if page.client_storage.get('access_token'):
-            headers = {
-                "Content-Type": "application/json",
-                "Authorization": f"Bearer {page.client_storage.get('access_token')}"
-            }
-        else:
-            headers = {
-                "Content-Type": "application/json",
-            }
-        response = requests.get(url=url, headers=headers)
-        if response.status_code == 200:
-            return response.json().get("result", [])
-        else:
-            print("ERROR")
-        return []
-
-
-    def scroll_to_item(item_id, chapter_id):
-
-        # Perform the necessary actions (e.g., highlight or take some action with chapter_id)
-        take_id(chapter_id, number=button_number)
-
-        # Find the target element
-        target_element = next((control for control in right_display.controls if control.key == item_id), None)
-
-        if target_element:
-            # Apply highlight style
-            original_bgcolor = target_element.controls[0].controls[0].bgcolor
-            target_element.controls[0].controls[0].bgcolor = "yellow"
-            target_element.update()
-
-            # Function to remove highlight after a delay
-            def remove_highlight():
-                # Sleep for 3 seconds
-                time.sleep(3)
-                # Restore original background color
-                target_element.controls[0].controls[0].bgcolor = original_bgcolor
-                target_element.update()
-
-        # Scroll to the target element
-        right_display.scroll_to(key=f"{item_id}", duration=700, curve=ft.AnimationCurve.BOUNCE_OUT)
-        remove_highlight()
-        page.update()
-
-    def handle_submit(e):
-        search.close_view(e.data)
-        query = e.data.strip()
-
-        if not query:
-            search.controls.clear()
-            search.update()
-            return
-
-        search_data = fetch_data(query)
-
-        search.controls.clear()
-
-        def create_on_click_handler(item_id, chapter_id):
-            return lambda e: scroll_to_item(item_id, chapter_id)
-
-        for search_detail in search_data:
-            item_id = search_detail.get('id')
-            chapter_id = search_detail.get('chapter_id')
-
-            # Pass both item_id and chapter_id as default arguments to the lambda function
-            list_tile = ft.ListTile(
-                key=item_id,
-                on_click=create_on_click_handler(item_id, chapter_id),
-                title=ft.Text(
-                    f"{search_detail.get('chapter_name')}, {search_detail.get('number')} - oyat"
-                )
-            )
-            search.controls.append(list_tile)
-
-        search.open_view()
-        search.update()  # Refresh the UI with the new data
-
-    search = ft.SearchBar(
-        width=180,
-        height=50,
-        expand=True,
-        bar_bgcolor="white",
-        bar_border_side=ft.BorderSide(color=ft.colors.BLUE, width=1),
-        divider_color=ft.colors.AMBER,
-        bar_leading=ft.Icon(ft.icons.SEARCH),
-        bar_hint_text="Nima o'qimoqchisiz?...",
-        view_hint_text="Searching...",
-        on_submit=handle_submit,  # Trigger search when user submits the query
-        controls=[],  # Start with an empty control list
-    )
-
-    routes = {
-        "Abu Mansur Motrudiy": al_quron_oquvchilariga,
-        "Tavilot al-Quron": surah_page,
-        "Qo'lyozma va sharhlar": menuscript,
-        "Zamonaviy tadqiqotlar": studies,
-        "Resurslar": resources,
-        "Mutaassib oqimlarga raddiyalar": refusal
-    }
-
-    active_route = None
-
-    def navigate(e, route):
-        nonlocal active_route
-        if route != active_route:
-            active_route = route
-            update_appbar()  # Refresh the AppBar with updated colors
-            if route in routes:
-                routes[route](page)  # Call the corresponding route function
-            else:
-                page.add(ft.Text("404 - Page Not Found", size=20))
-                page.update()
-
-    def generate_appbar_actions():
-        return [
-            ft.TextButton(
-                adaptive=True,
-                expand=True,
-                text=route_label,
-                style=ft.ButtonStyle(
-                    padding=0,
-                    text_style=ft.TextStyle(size=15),
-                    color='#007577' if route == active_route else ft.colors.BLACK,
-                ),
-                on_click=lambda e, r=route: navigate(e, r)
-            )
-            for route, route_label in [
-                ("Abu Mansur Motrudiy", "Abu Mansur Motrudiy"),
-                ("Tavilot al-Quron", "Tavilot al-Quron"),
-                ("Qo'lyozma va sharhlar", "Qo'lyozma va sharhlar"),
-                ("Zamonaviy tadqiqotlar", "Zamonaviy tadqiqotlar"),
-                ("Resurslar", "Resurslar"),
-                ("Mutaassib oqimlarga raddiyalar", "Mutaassib oqimlarga raddiyalar")
-
-            ]
-        ]
-
-    def update_appbar():
-        page.appbar = ft.AppBar(
-            title=ft.Row(
-                alignment=ft.MainAxisAlignment.CENTER,
-                expand=True,
-                adaptive=True,
-                controls=[
-                    *generate_appbar_actions(),
-                ]
-            ),
-            center_title=True,
-            adaptive=True,
-            leading_width=100,
-            leading=ft.Container(
-                on_click=lambda e: home(page),
-                content=ft.Image(
-                    expand=True,
-                    color='#007577',
-                    src=os.path.abspath("assets/tA'VILOT_Монтажная_область1.svg")
-                )),
-            actions=[
-                ft.Row(
-                    adaptive=True,
-                    expand=True,
-                    alignment=ft.MainAxisAlignment.CENTER,
-                    spacing=15,  # Reduced spacing to allow more room for items
-                    controls=[
-                        search
-                    ],
-                ),
-            ],
-            bgcolor='white',
-            toolbar_height=80,
-        )
-
-    update_appbar()
+    # def fetch_data(query):
+    #     url = f"http://176.221.28.202:8008/api/v1/search/?q={query}&search_type={button_number}"
+    #     headers = ""
+    #     if page.client_storage.get('access_token'):
+    #         headers = {
+    #             "Content-Type": "application/json",
+    #             "Authorization": f"Bearer {page.client_storage.get('access_token')}"
+    #         }
+    #     else:
+    #         headers = {
+    #             "Content-Type": "application/json",
+    #         }
+    #     response = requests.get(url=url, headers=headers)
+    #     if response.status_code == 200:
+    #         return response.json().get("result", [])
+    #     else:
+    #         print("ERROR")
+    #     return []
+    #
+    #
+    # def scroll_to_item(item_id, chapter_id):
+    #
+    #     # Perform the necessary actions (e.g., highlight or take some action with chapter_id)
+    #     take_id(chapter_id, number=button_number)
+    #
+    #     # Find the target element
+    #     target_element = next((control for control in right_display.controls if control.key == item_id), None)
+    #
+    #     if target_element:
+    #         # Apply highlight style
+    #         original_bgcolor = target_element.controls[0].controls[0].bgcolor
+    #         target_element.controls[0].controls[0].bgcolor = "yellow"
+    #         target_element.update()
+    #
+    #         # Function to remove highlight after a delay
+    #         def remove_highlight():
+    #             # Sleep for 3 seconds
+    #             time.sleep(3)
+    #             # Restore original background color
+    #             target_element.controls[0].controls[0].bgcolor = original_bgcolor
+    #             target_element.update()
+    #
+    #     # Scroll to the target element
+    #     right_display.scroll_to(key=f"{item_id}", duration=700, curve=ft.AnimationCurve.BOUNCE_OUT)
+    #     remove_highlight()
+    #     page.update()
+    #
+    # def handle_submit(e):
+    #     search.close_view(e.data)
+    #     query = e.data.strip()
+    #
+    #     if not query:
+    #         search.controls.clear()
+    #         search.update()
+    #         return
+    #
+    #     search_data = fetch_data(query)
+    #
+    #     search.controls.clear()
+    #
+    #     def create_on_click_handler(item_id, chapter_id):
+    #         return lambda e: scroll_to_item(item_id, chapter_id)
+    #
+    #     for search_detail in search_data:
+    #         item_id = search_detail.get('id')
+    #         chapter_id = search_detail.get('chapter_id')
+    #
+    #         # Pass both item_id and chapter_id as default arguments to the lambda function
+    #         list_tile = ft.ListTile(
+    #             key=item_id,
+    #             on_click=create_on_click_handler(item_id, chapter_id),
+    #             title=ft.Text(
+    #                 f"{search_detail.get('chapter_name')}, {search_detail.get('number')} - oyat"
+    #             )
+    #         )
+    #         search.controls.append(list_tile)
+    #
+    #     search.open_view()
+    #     search.update()  # Refresh the UI with the new data
+    #
+    # search = ft.SearchBar(
+    #     width=180,
+    #     height=50,
+    #     expand=True,
+    #     bar_bgcolor="white",
+    #     bar_border_side=ft.BorderSide(color=ft.colors.BLUE, width=1),
+    #     divider_color=ft.colors.AMBER,
+    #     bar_leading=ft.Icon(ft.icons.SEARCH),
+    #     bar_hint_text="Nima o'qimoqchisiz?...",
+    #     view_hint_text="Searching...",
+    #     on_submit=handle_submit,  # Trigger search when user submits the query
+    #     controls=[],  # Start with an empty control list
+    # )
+    update_appbar(page)
     page.add(side_bar)
-    # page.update()
